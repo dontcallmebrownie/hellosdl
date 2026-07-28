46a06501542390aaf49a31b06ec20eb4cbbf6de4
David Cramer <dontcallmebrownie@gmail.com>
2026-07-27 22:59:08 -0500

    installed SDL as a submodule and I think i correctly configured the CMake build system

---
+++ src/CMakeFiles/hellosdl.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../hellosdl"
+  "../hellosdl.pdb"
+  "CMakeFiles/hellosdl.dir/hello.c.o"
+  "CMakeFiles/hellosdl.dir/hello.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hellosdl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
